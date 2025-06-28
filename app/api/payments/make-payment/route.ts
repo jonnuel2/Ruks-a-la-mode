3d7e504bd9c9685c7a4acfa11877957592001074
jonnuel2 <nonsokach@gmail.com>
2025-06-28 14:37:20 +0100

    routes/make-payment line 21

--- app/api/payments/make-payment/route.ts
+++ app/api/payments/make-payment/route.ts
@@ -17,7 +17,9 @@ export async function POST(req: NextRequest) {
       "https://api.paystack.co/transaction/initialize",
       {
         email,
-        amount: price * 100, // Amount in kobo
+        // amount: price * 100, // Amount in kobo
+        amount: Math.round(price * 100), // ✅ always a whole number
+
         callback_url: callbackUrl,
         currency,
       },
